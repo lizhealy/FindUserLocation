4660998eca717015b6df7fd0b425824b04369370
lizhealy <lizhealy8@gmail.com>
2017-01-18 13:12:12 -0500

    Initial Commit

---
+++ Find User Location/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Find User Location
+//
+//  Created by Liz Healy on 1/18/17.
+//  Copyright © 2017 netGalaxy Studios. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
