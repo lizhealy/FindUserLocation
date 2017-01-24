4d7fc343abb3814c546babf830c4499447f7d7a6
lizhealy <lizhealy8@gmail.com>
2017-01-24 12:42:54 -0500

    Initial Commit

--- Find User Location/ViewController.swift
+++ Find User Location/ViewController.swift
@@ -7,17 +7,89 @@
 //
 
 import UIKit
+import CoreLocation
+import MapKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
+    
+    @IBOutlet weak var map: MKMapView!
+    
+    var locationManager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        let userLocation: CLLocation = locations[0]
+        let latitude = userLocation.coordinate.latitude
+        let longitude = userLocation.coordinate.longitude
+        let latDelta: CLLocationDegrees = 0.05
+        let lonDelta: CLLocationDegrees = 0.05
+        let span: MKCoordinateSpan = MKCoordinateSpan(latitudeDelta: latDelta, longitudeDelta: lonDelta)
+        let coordinates = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+        let region: MKCoordinateRegion = MKCoordinateRegion(center: coordinates, span: span)
+        
+        self.map.setRegion(region, animated: true)
+        
+        CLGeocoder().reverseGeocodeLocation(userLocation) { (placemarks, error) in
+            
+            if error != nil {
+                print(error)
+            }
+            else {
+                if let placemark = placemarks?[0] {
+                    var subThoroughfare = ""
+                    if placemark.subThoroughfare != nil {
+                        subThoroughfare = placemark.subThoroughfare!
+                    }
+                    var thoroughfare = ""
+                    if placemark.thoroughfare != nil {
+                        thoroughfare = placemark.thoroughfare!
+                    }
+                    var subLocality = ""
+                    if placemark.subLocality != nil {
+                        subLocality = placemark.subLocality!
+                    }
+                    var subAdministrativeArea = ""
+                    if placemark.subAdministrativeArea != nil {
+                        subAdministrativeArea = placemark.subAdministrativeArea!
+                    }
+                    
+                    var administrativeArea = ""
+                    if placemark.administrativeArea != nil {
+                        administrativeArea = placemark.administrativeArea!
+                    }
+                    var postalCode = ""
+                    if placemark.postalCode != nil {
+                        postalCode = placemark.postalCode!
+                    }
+                    var country = ""
+                    if placemark.country != nil {
+                        country = placemark.country!
+                    }
+                    
+                    print(subThoroughfare + " " + thoroughfare + "\n" + subLocality + "\n" + subAdministrativeArea + ", " + administrativeArea + "\n" + postalCode + "\n" + country)
+                }
+            }
+            
+        }
+        
+        
     }
 
 
